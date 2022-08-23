91009521709e3077402a7df0edb2fde9e5f55b7f
sanchadr002 <sanchadr002@gmail.com>
2022-08-22 19:02:46 -0700

    add navbar to application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, { Fragment } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
+import Navbar from './components/navigation/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Fragment>
+      <Navbar />
+    </Fragment>
   );
 }
 
---
+++ src/components/navigation/Navbar.js
@@ -0,0 +1,37 @@
+// Import dependencies
+import React from 'react'
+import { Link } from "react-router-dom"
+import styled from 'styled-components'
+
+const Wrapper = styled.section({
+    padding: '1em',
+    background: 'lightblue',
+    display: 'flex'
+})
+
+const NavItem = styled.span({
+    padding: '1em',
+    
+})
+
+const Navbar = () => {
+    
+    return (
+        <Wrapper>
+            <NavItem>
+                <Link to="/">Home</Link>
+            </NavItem>
+            <NavItem>
+                <Link to="/guilds">The Guilds</Link>
+            </NavItem>
+            <NavItem>
+                <Link to="/campaign">Campaign Concept</Link>
+            </NavItem>
+            <NavItem>
+                <Link to="/character">Character Creation</Link>
+            </NavItem>
+        </Wrapper>
+    )
+}
+
+export default Navbar
